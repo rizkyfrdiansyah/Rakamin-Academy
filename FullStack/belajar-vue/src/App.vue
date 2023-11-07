15e3d7f7ac5814de010cc15ab8d0e425a2af61de
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-07 22:11:30 +0700

    coba input nama

--- FullStack/belajar-vue/src/App.vue
+++ FullStack/belajar-vue/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input type="text" v-model="name" />
+  <h4>Name: {{ name }}</h4>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
