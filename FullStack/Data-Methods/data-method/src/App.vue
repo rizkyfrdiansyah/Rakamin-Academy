ba35b96954e1ece36c643db2d6eb3f44f5a3c94c
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-07 22:36:08 +0700

    button name

---
+++ FullStack/Data-Methods/data-method/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Name: {{ name }}</h2>
+  <button v-on:click="changeName">Change Name</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "M Rizky Ferdiansyah",
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "Bulskey";
+    },
+  },
+};
+</script>
+
+<style></style>
