089fb3a957468cf85dbf69fa23270af128178797
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-08 14:42:06 +0700

    done

---
+++ FullStack/data-binding/data-binding/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <a v-bind:href="url">GO TO BULSKEY.COM</a>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      url: "https://bulskey.com",
+    };
+  },
+};
+</script>
+
+<style></style>
