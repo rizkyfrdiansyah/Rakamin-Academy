47dcbbc20f1d707625ada791eae8ab73dc36913c
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-11 23:56:29 +0700

    done

---
+++ FullStack/array/array/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <div>
+      <ol>
+        <li v-for="(food, index) in foods" :key="index">
+          {{ food }}
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      foods: ["Nasi Goreng", "Lontong Sayur", "Bakso", "Mie Ayam", "Ketoprak"],
+    };
+  },
+};
+</script>
